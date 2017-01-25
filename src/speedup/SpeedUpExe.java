1fa2bd4024fa73e192228bc5ec017984e0f5aaae
Vzzarr <nich.tucci@gmail.com>
2017-01-25 16:13:01 +0100

    last commit

--- src/speedup/SpeedUpExe.java
+++ src/speedup/SpeedUpExe.java
@@ -8,12 +8,12 @@ public class SpeedUpExe {
 		SpeedUp3FJ su3 = new SpeedUp3FJ();
 		SpeedUp4SI su4 = new SpeedUp4SI();
 		
-		/*System.out.println("SpeedUp HWJ1");
+		System.out.println("SpeedUp HWJ1");
 		su1.execute(15, "complete");
 		System.out.println("\n\n\nSpeedUp HWJ2");
 		su2.execute(15, "complete");
 		System.out.println("\n\n\nSpeedUp HWJ3");
-		su3.execute(15, "complete");*/
+		su3.execute(15, "complete");
 		System.out.println("\n\n\nSpeedUp HWJ4");
 		su4.execute(15, "complete");
 
